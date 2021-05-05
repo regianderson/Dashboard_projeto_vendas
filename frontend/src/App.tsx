480d0c955c323b78d02ea78b4945eec00eeb9651
Regianderson Souza <regiansouza39@gmail.com>
2021-05-05 00:12:22 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "componentes/DataTable";
+import Footer from "componentes/Footer";
+import NavBar from "componentes/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
