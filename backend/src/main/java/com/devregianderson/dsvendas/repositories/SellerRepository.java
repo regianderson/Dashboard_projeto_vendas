3368de6e7191b7a50dfd347afd08f264a7627c34
Regianderson Souza <regiansouza39@gmail.com>
2021-05-05 23:51:55 -0300

    Layers

---
+++ backend/src/main/java/com/devregianderson/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devregianderson.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devregianderson.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository <Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/devregianderson/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.devregianderson.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devregianderson.dsvendas.dto.SellerDTO;
+import com.devregianderson.dsvendas.entities.Seller;
+import com.devregianderson.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
